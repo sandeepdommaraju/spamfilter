02ce4b27020a84289b1e040341f980981c91ebde
sandeep <sandeepdommaraju@gmail.com>
2016-04-27 06:20:21 -0400

    MultinomialNB, BernoulliNB classifiers are compared with various cross-validation sets

--- NB.py
+++ NB.py
@@ -6,10 +6,11 @@ import os
 import numpy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.cross_validation import KFold
-from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
 
 txtpath = '/home/sunito/Desktop/spamdata/TEXT/'
 respath = '/home/sunito/Desktop/out/'
@@ -47,45 +48,71 @@ data = DataFrame({'content': [], 'class': []})
 data = data.append(createframes(txtpath + 'ham/', hamtxtfiles, 'ham'))
 data = data.append(createframes(txtpath + 'spam/', spamtxtfiles, 'spam'))
 
-print('shuffle the data... \n')
+print('shuffle the data...')
 #data = data.reindex(numpy.random.permutation(data.index))
 #data.to_csv(respath + 'dataframes')
 
-print('tokenizing..using wordfrequency')
-countvectorizer = CountVectorizer()
-wfqs = countvectorizer.fit_transform(data['content'].values)
+print('\n')
+print('creating pipeline (bag of words) -> (MultinomialNB)')
+pipeline_bag_mnb = Pipeline([('vectorizer', CountVectorizer()), ('classifier', MultinomialNB())])
 
-clf_MNB = MultinomialNB()
-labels = data['class'].values
-clf_MNB.fit(wfqs, labels)
+print('creating pipeline (BIGRAMS) -> (MultinomialNB)')
+pipeline_bag_bg_mnb = Pipeline([('vectorizer', CountVectorizer(ngram_range=(1,2))), ('classifier', MultinomialNB())])
 
-print('create pipline (word frequency tokenizer) -> (MultinomialNB)')
-pipeline = Pipeline([('vectorizer', CountVectorizer()), ('classifier', MultinomialNB())])
+print('creating pipeline (TF-IDF) -> (MultinomialNB)')
+pipeline_tfidf_mnb = Pipeline([('vectorizer', TfidfVectorizer()), ('classifier', MultinomialNB())])
 
-pipeline.fit(data['content'].values, data['class'].values)
+print('creating pipeline (bag of words) -> (BernoulliNB)')
+pipeline_bag_bnb = Pipeline([('vectorizer', CountVectorizer()), ('classifier', BernoulliNB())])
 
-print('KFold cross validation...')
+print('creating pipeline (BIGRAMS) -> (BernoulliNB)')
+pipeline_bag_bg_bnb = Pipeline([('vectorizer', CountVectorizer(ngram_range=(1,2))), ('classifier', BernoulliNB())])
 
-k_fold= KFold(n=len(data), n_folds=6)
-scores = []
-confusion = numpy.array([[0,0], [0,0]])
+print('creating pipeline (TF-IDF) -> (BernoulliNB)')
+pipeline_tfidf_bnb = Pipeline([('vectorizer', TfidfVectorizer()), ('classifier', BernoulliNB())])
 
-for train_indices, test_indices in k_fold:
-    train_text = data.iloc[train_indices]['content'].values
-    train_y = data.iloc[train_indices]['class'].values
+print('creating pipeline (BI-GRAMS) -> (TF-IDF) -> (MultinomialNB)')
+pipeline_bg_tfidf_mnb = Pipeline([('vectorizer', CountVectorizer(ngram_range=(1,2))), ('tf-idf_transformer', TfidfTransformer()), ('classifier', MultinomialNB())])
 
-    test_text = data.iloc[test_indices]['content'].values
-    test_y = data.iloc[test_indices]['class'].values
 
-    pipeline.fit(train_text, train_y)
-    predictions = pipeline.predict(test_text)
+#pipeline.fit(data['content'].values, data['class'].values)
 
-    confusion += confusion_matrix(test_y, predictions)
-    score = f1_score(test_y, predictions, pos_label='spam')
-    scores.append(score)
+def crossvalidate(pipeline, method, classifier, folds):
+    print('Using ' + method + ' as features and ' + classifier)
+    print('Running cross validation with ' + str(folds) + ' folds ....')
+    k_fold= KFold(n=len(data), n_folds=folds)
+    scores = []
+    confusion = numpy.array([[0,0], [0,0]])
 
-print('Total emails classified:', len(data))
-print('Score:', sum(scores)/len(scores))
-print('Confusion matrix:')
-print(confusion)
+    for train_indices, test_indices in k_fold:
+        train_text = data.iloc[train_indices]['content'].values
+        train_y = data.iloc[train_indices]['class'].values
+        
+        test_text = data.iloc[test_indices]['content'].values
+        test_y = data.iloc[test_indices]['class'].values
+	print('train set: ' + str(len(train_y)))
+	print('test  set:  ' + str(len(test_y)))
+        pipeline.fit(train_text, train_y)
+        predictions = pipeline.predict(test_text)
+
+        confusion += confusion_matrix(test_y, predictions)
+        #score = f1_score(test_y, predictions, pos_label='spam')
+        score = accuracy_score(test_y, predictions)
+        scores.append(score)
+
+    #print('\n')
+    #print('Total emails classified: ' + str(len(data)))
+    print('Accuracy: '+ str(sum(scores)/len(scores)))
+    print('Confusion matrix:')
+    print(confusion)
+    print('\n')
+
+print('\n')
+crossvalidate(pipeline_bag_mnb, 'BAG OF WORDS', 'MultinomialNaiveBayes Classifier', 8)
+crossvalidate(pipeline_bag_bg_mnb, 'BI-GRAMS', 'MultinomialNaiveBayes Classifier', 8)
+crossvalidate(pipeline_tfidf_mnb, 'TF-IDF', 'MultinomialNaiveBayes Classifier', 8)
+crossvalidate(pipeline_bag_mnb, 'BAG OF WORDS', 'BernoulliNaiveBayes Classifier', 8)
+crossvalidate(pipeline_bag_bg_mnb, 'BI-GRAMS', 'BernoulliNaiveBayes Classifier', 8)
+crossvalidate(pipeline_tfidf_mnb, 'TF-IDF', 'BernoulliNaiveBayes Classifier', 8)
+crossvalidate(pipeline_bg_tfidf_mnb, 'BIGRAMS and TF-IDF', 'BernoulliNaiveBayes Classifier', 8)
 
